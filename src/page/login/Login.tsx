1cec457876976cdbeb9cb6ec0d990def7de1cb5d
miklosabel <miklosabel@gmail.com>
2020-06-12 12:39:17 +0200

    form validation added

--- src/page/login/Login.tsx
+++ src/page/login/Login.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { findRenderedDOMComponentWithClass } from 'react-dom/test-utils';
 
 interface State {
   username: string;
   password: string;
+  errors: {
+    username: string;
+    password: string;
+  };
 }
 
 export class Login extends React.Component<{}, State> {
@@ -12,11 +17,16 @@ export class Login extends React.Component<{}, State> {
     this.state = {
       username: '',
       password: '',
+      errors: {
+        username: '',
+        password: '',
+      },
     };
   }
   // handlechange == changes the state
   handleChange(value: string, type: string) {
     this.setState({
+      //TODO miert nme jo ha csak a value-t adom at es azt egyenlove teszem?
       ...this.state,
       [type]: value,
     });
@@ -24,16 +34,50 @@ export class Login extends React.Component<{}, State> {
   // handlesubmit == submits the data -- this posts data to backend -- in this case logs the data
   handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    this.validateForm();
+    console.log(this);
   }
+
+  validateForm() {
+    let username = this.state.username;
+    let password = this.state.password;
+    let errors = {
+      username: '',
+      password: '',
+    };
+    let formIsValid = true;
+
+    if (!username) {
+      formIsValid = false;
+      errors.username= "*Kötelező mező";
+    }
+    if (!password) {
+      formIsValid = false;
+      errors.password= "*Kötelező mező";
+    }
+    // if (typeof password !== "undefined") {
+    //   if (!password.match(/^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&]).*$/)) {
+    //     formIsValid = false;
+    //     errors.password = "*Erős és biztonságos jelszót adj meg";
+    //   }
+    // }
+    this.setState({
+      errors: errors
+    });
+    return formIsValid;
+  }
+
   render() {
     return (
       <div>
         <header>
+          <div className="header-line"></div>
           <h1 className="login-title">UNDERSEA</h1>
         </header>
         <div className="form-container">
           <h2 className="form-title">Belépés</h2>
           <form onSubmit={(event) => this.handleSubmit(event)}>
+            <div className="error-message">{this.state.errors.username}</div>
             <input
               type="text"
               name="username"
@@ -43,6 +87,7 @@ export class Login extends React.Component<{}, State> {
               }
             />
             <br />
+            <div className="error-message">{this.state.errors.password}</div>
             <input
               type="password"
               name="password"
@@ -57,11 +102,11 @@ export class Login extends React.Component<{}, State> {
             </button>
           </form>
 
-          <div className="no-bg-button">
+          {/* <div className="no-bg-button"> */}
             <Link className="no-bg-button" to="./register">
               Regisztráció
             </Link>
-          </div>
+          {/* </div> */}
         </div>
       </div>
     );
