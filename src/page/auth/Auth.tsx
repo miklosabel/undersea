5a402a7e67ea7c888380dd6ebe556149d6edde6c
miklosabel <miklosabel@gmail.com>
2020-06-16 19:17:05 +0200

    Empty AuthComponent added

---
+++ src/page/auth/Auth.tsx
@@ -0,0 +1,15 @@
+//TODO auth page 
+//TODO mocking
+//TODO learn react components states
+import React from "react";
+
+interface State {
+  isLoading: boolean;
+  result: boolean;
+}
+
+
+export class AuthComponent extends React.Component<{}, State> {
+  // TODO after a cartain amount of time randomly allow or deny login
+  //TODO render returns a redirect to logged in page if login allowed 
+}
