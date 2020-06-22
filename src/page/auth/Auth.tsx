f86a905e65b736b56f2dbb2cb5cfbafeb8ae6de8
miklosabel <miklosabel@gmail.com>
2020-06-22 23:01:16 +0200

    try to make protected route

--- src/page/auth/Auth.tsx
+++ src/page/auth/Auth.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
-import { Main } from './Main';
+import React, { useReducer } from 'react';
+import {
+  Redirect,
+  Route,
+  RouteComponentProps,
+  withRouter,
+} from 'react-router-dom';
+import Main from './Main';
 
 interface State {
   isLoading: boolean;
   loginSuccess?: boolean;
 }
-interface Props {
+interface Props extends RouteComponentProps {
   loginMsg: (msg: string) => void;
 }
 
@@ -21,13 +26,18 @@ class AuthComponent extends React.Component<Props, State> {
 
   componentDidMount() {
     setTimeout(() => {
-      // const number = Math.floor(Math.random() * 11);
-      const number = 2;
+      const number = Math.floor(Math.random() * 11);
+      // const number = 1;
       if (number > 7) {
         this.setState({ ...this.state, isLoading: false, loginSuccess: false });
         this.props.loginMsg('*Nem megfelelő felhasználónév vagy jelszó');
       } else {
-        this.setState({ ...this.state, isLoading: false, loginSuccess: true });
+        this.setState(
+          { ...this.state, isLoading: false, loginSuccess: true },
+          () => {
+            this.props.history.push('/main');
+          }
+        );
       }
     }, 2000);
   }
@@ -42,13 +52,11 @@ class AuthComponent extends React.Component<Props, State> {
             {this.state.loginSuccess === false && (
               <Redirect from="*" to="/login"></Redirect>
             )}
-            <Route path="/main">
-              <Main></Main>
-            </Route>
+            <Route path="/main" component={Main}/>
           </>
         )}
       </>
     );
   }
 }
-export default AuthComponent;
+export default withRouter(AuthComponent);
