96af2ff1cd87119fffe219e871a3bb0166fdea99
miklosabel <miklosabel@gmail.com>
2020-06-24 13:38:52 +0200

    status bar is ok except some css

--- src/components/statusBar/StatusBar.tsx
+++ src/components/statusBar/StatusBar.tsx
@@ -1,6 +1,76 @@
 import React from 'react';
 import './StatusBar.scss';
+import SealPng from '../../assets/seal.png';
+import SharkPng from '../../assets/shark.png';
+import CoralPng from '../../assets/coral.png';
+import ShellPng from '../../assets/shell.png';
+import SeahorsePng from '../../assets/seahorse.png';
+import BuildingsPng from '../../assets/buildings.png';
+import UpgradesPng from '../../assets/upgrades.png';
 
-export default class StatusBar extends React.Component<{},{}>{
+interface Props {
+  round?: number;
+  rank?: number;
+  shark?: number;
+  seal?: number;
+  seahorse?: number;
+  shell?: number;
+  shellIncome?: number;
+  coral?: number;
+  coralIncome?: number;
+  buildings?: number;
+  isBuilding?: boolean;
+  upgrades?: number;
+  isUpgrading?: boolean;
+}
 
+export default class StatusBar extends React.Component<Props, {}> {
+  render() {
+    return (
+      <div className="statusbar">
+        <p>{this.props.round}. kör</p>
+        <p>{this.props.rank}. hely</p>
+        <div className="counter">
+          <img src={SharkPng} alt="shark" />
+          <p>{this.props.shark}</p>
+        </div>
+        <div className="counter">
+          <img src={SealPng} alt="seal" />
+          <p>{this.props.seal}</p>
+        </div>
+        <div className="counter">
+          <img src={SeahorsePng} alt="seahorse" />
+          <p>{this.props.seahorse}</p>
+        </div>
+        <div className="counter">
+          <img src={ShellPng} alt="shell" />
+          <div>
+            <p>{this.props.shell}</p>
+            <p>{this.props.shellIncome}/kör</p>
+          </div>
+        </div>
+        <div className="counter">
+          <img src={CoralPng} alt="coral" />
+          <div>
+            <p>{this.props.coral}</p>
+            <p>{this.props.coralIncome}/kör</p>
+          </div>
+        </div>
+        <div className="counter">
+          <img src={BuildingsPng} alt="buildings" />
+          <div>
+            <p>{this.props.buildings}</p>
+            {this.props.isBuilding ? <p>1 épül</p> : <p>0 épül</p>}
+          </div>
+        </div>
+        <div className="counter">
+          <img src={UpgradesPng} alt="upgrades" />
+          <div>
+            <p>{this.props.upgrades}</p>
+            {this.props.isUpgrading ? <p>1 épül</p> : <p>0 épül</p>}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
--- src/page/auth/Main.tsx
+++ src/page/auth/Main.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import './Main.scss';
 import TowerImage from '../../components/towerImage/TowerImage';
+import StatusBar from '../../components/statusBar/StatusBar';
 
 export default class Main extends React.Component<{}, {}> {
   render() {
     return (
-      <>
+      <div className="main-container">
+        <StatusBar
+          round={5}
+          rank={5}
+          shark={3}
+          seal={2}
+          seahorse={43}
+          shell={112}
+          shellIncome={20}
+          coral={10}
+          coralIncome={5}
+          buildings={0}
+          upgrades={0}
+        />
         <TowerImage />
-      </>
+      </div>
     );
   }
 }
