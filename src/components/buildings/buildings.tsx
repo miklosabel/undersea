598becf409277e114f0bd547c07a1d27b509ba31
miklos abel <miklosabel@gmail.com>
2020-07-07 17:53:55 +0200

    build component

---
+++ src/components/buildings/buildings.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface Props {
+
+}
+
+export class Buildings extends React.Component<Props, {}> {
+  render() {
+    return (
+      <div className="buildings">
+        
+      </div>
+    )
+  }
+
+}
