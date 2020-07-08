18305d086499d0a25f236cceee68904cddc43bc8
miklos abel <miklosabel@gmail.com>
2020-07-08 18:39:21 +0200

    buildings added, styling

--- src/components/buildings/buildings.tsx
+++ src/components/buildings/buildings.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import './buildings.scss'
 import {
   COST_OF_ATOLLFORTRESS,
   COST_OF_FLOWCONTROLLER,
 } from '../../mock/contants';
+import atollFortressImage from '../../assets/atollfortress.png';
+import flowController from '../../assets/flowcontroller.png';
 
 interface Props {
   atollFortress: number;
@@ -15,10 +18,10 @@ export class Buildings extends React.Component<Props, {}> {
       <div className="buildings">
         <h1>Épületek</h1>
         <p className="strong">Kattings rá, amelyiket szeretnéd megvenni</p>
-        <p>Egyszerre csakegy épület épülhet</p>
+        <p>Egyszerre csak egy épület épülhet</p>
         <div className="card-container">
           <div className="card">
-            <img src="" alt="zatonyvar" />
+            <img src={atollFortressImage} alt="zatonyvar" />
             <p className="strong">Zátonyvár</p>
             <p className="strong">50 embert ad a népességhez</p>
             <p className="strong">200 krumplit termel körönként</p>
@@ -26,14 +29,14 @@ export class Buildings extends React.Component<Props, {}> {
             <p>{COST_OF_ATOLLFORTRESS} Gyöngy / db</p>
           </div>
           <div className="card">
-            <img src="" alt="aramlasiranyito" />
+            <img src={flowController} alt="aramlasiranyito" />
             <p className="strong">Áramlásirányító</p>
-            <p className="strong">200 egységneks nyújt szállást</p>
+            <p className="strong">200 egységnek nyújt szállást</p>
             <p>{this.props.flowController} db</p>
             <p>{COST_OF_FLOWCONTROLLER} Gyöngy / db</p>
           </div>
         </div>
-        <button>Megveszem</button>
+        <button>dolgok</button>
       </div>
     );
   }
