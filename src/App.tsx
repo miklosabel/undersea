520966469e25f5f782038fb2c03cabf9ae90a097
miklosabel <miklosabel@gmail.com>
2020-08-13 12:35:02 +0200

    get rid of all unneccessary general.scss imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,28 @@
 import React, { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
-import './styles/general.scss';
-import { Switch, Route, Redirect, } from 'react-router';
+import { Switch, Route, Redirect } from 'react-router';
 import { createBrowserHistory } from 'history';
 import { Register } from './page/register/Register';
 import { AuthComponentConnected } from './page/auth/connect';
 
 function App(props: { init: (isConnected: boolean) => void }) {
-
   useEffect(() => {
     //Kiolvassuk, volt-e bejelentkezve
-    const isConnectedUnparsed = localStorage.getItem('isConnected')
-    const isConnected = isConnectedUnparsed ? JSON.parse(isConnectedUnparsed) : false
+    const isConnectedUnparsed = localStorage.getItem('isConnected');
+    const isConnected = isConnectedUnparsed
+      ? JSON.parse(isConnectedUnparsed)
+      : false;
     //Ha nem volt false értékkel elküldjük action
-    props.init(isConnected)
-
-  })
+    props.init(isConnected);
+  });
   return (
     <div className="App">
       <Switch>
         <Route path="/register">
           <Register></Register>
         </Route>
-        AuthComponentConnected
-        />
+        <AuthComponentConnected />
         <Redirect from="*" to="/login"></Redirect>
       </Switch>
     </div>
