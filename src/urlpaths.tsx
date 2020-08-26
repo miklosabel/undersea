7ed29c9f8caa211e03eae67df0fac7ac41f97541
miklosabel <miklosabel@gmail.com>
2020-08-26 12:14:49 +0200

    added urlpaths

---
+++ src/urlpaths.tsx
@@ -0,0 +1,5 @@
+export const registerUrlPath: string = "/register";
+export const loginUrlPath: string = "/login";
+export const mainUrlPath: string = "/main";
+export const buildingsUrlPath: string = '/main/buildings';
+export const attacksUrlPath: string = '/main/attacks';
