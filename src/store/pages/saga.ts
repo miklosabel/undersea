ea0fdb9f1f3dfd13d16e647e3900b4aba9989e86
miklosabel <miklosabel@gmail.com>
2020-09-02 14:34:00 +0200

    adding watch to saga

--- src/store/pages/saga.ts
+++ src/store/pages/saga.ts
@@ -1,11 +1,14 @@
 import { all } from 'redux-saga/effects';
 
 import { watchInitResponseAction, watchLoginAction, watchLoginResponseAction } from './login/saga';
+import { watchBuildingRequestAction, watchBuildingResponseAction } from './buildings/saga';
 
 export function* pageSaga() {
   yield all([
     watchLoginAction(),
     watchLoginResponseAction(),
     watchInitResponseAction(),
+    watchBuildingRequestAction(),
+    watchBuildingResponseAction(),
   ]);
 }
